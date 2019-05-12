92ce0a56ec42b9d5a1effb0fd49f3e18be9b1a52
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-05-12 13:04:11 -0500

    Actualización quinto ejercicio

---
+++ src/cinco.java
@@ -0,0 +1,19 @@
+public class cinco {
+    public static void main (String[] args){
+        //Mensaje de bienvenida
+        System.out.print("Hola! Vamos a imprimir el resultado de varios procedimientos matemáticos!\n");
+        //Creamos los respectivos procedimientos matemáticos
+        int resultado1=-5+8*6;
+        int resultado2=(55+9)%9;
+        int resultado3=20+(-3*5)/8;
+        int resultado4=5+15/3*2-8%3;
+        //Imprimimos los resultados
+        System.out.println("Resultado 1: "+ resultado1);
+        System.out.println("Resultado 2: "+ resultado2);
+        System.out.println("Resultado 3: "+ resultado3);
+        System.out.println("Resultado 4: "+ resultado4);
+        System.out.print("Gracias por utilizar el programa");
+
+    }
+}
+//Desarrollado por Pedro Gómez , Duvan , Sebastián Gelves y Wilson
