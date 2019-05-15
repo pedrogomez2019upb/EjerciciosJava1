e4bf379fe50fb6c4c6b289f749873f4bd405a6b8
pedrogomez2019upb <47305995+pedrogomez2019upb@users.noreply.github.com>
2019-05-15 15:34:40 -0500

    Se añadió el ejercicio 26

---
+++ src/veintiseis.java
@@ -0,0 +1,22 @@
+public class veintiseis {
+    public static void main(String[] args){
+        System.out.print("Hola! Vamos a evaluar valores booleanos \n");
+        //Creamos los variables booleanas y mandamos la respuesta
+        boolean a=true || false;
+        boolean b=false || false;
+        boolean c=true && true;
+        boolean d=false&&false;
+        boolean e=(false&&false)||(true&&true);
+        boolean f=(false||false)&&(true&&true);
+        //Imprimimos los resultados booleanos
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.print("Gracias por utilizar el programa.");
+
+    }
+}
+//Desarrollado por Pedro Gómez , Duvan , Sebastián Gelves y Wilson
